a20302593f307821e42de391ee3911a7d24d9e55
elizeuangelo <elizeu90@gmail.com>
2022-09-01 11:20:57 -0300

    added dungeondraft tint filter

---
+++ tokenmagic/fx/filters/FilterDDTint.js
@@ -0,0 +1,27 @@
+import { customVertex2D } from '../glsl/vertexshaders/customvertex2D.js';
+import { CustomFilter } from './CustomFilter.js';
+import { Anime } from "../Anime.js";
+import "./proto/FilterProto.js";
+import { ddTint } from '../glsl/fragmentshaders/ddtint.js';
+
+export class FilterDDTint extends CustomFilter {
+  constructor(params) {
+    super(customVertex2D, ddTint);
+    this.tint = [1,0,0];
+    this.zOrder = 100;
+    this.animating = {};
+    this.setTMParams(params);
+    if (!this.dummy) {
+      this.anime = new Anime(this);
+      this.normalizeTMParams();
+    }
+  }
+
+  get tint() {
+    return this.uniforms.tint;
+  }
+
+  set tint(value) {
+    this.uniforms.tint = value;
+  }
+}
---
+++ tokenmagic/fx/glsl/fragmentshaders/ddtint.js
@@ -0,0 +1,17 @@
+export const ddTint = `
+varying vec2 vTextureCoord;
+uniform sampler2D uSampler;
+uniform vec3 tint;
+void main() {
+    vec4 color = texture2D(uSampler, vTextureCoord);
+    float lesser = min(color.g,color.b);
+    float bigger = max(color.g,color.b);
+    if ((bigger - lesser < 0.1) && (color.r > 1.5 * bigger)) {
+        float primary = color.r;
+        float range = primary - lesser;
+        color.r = lesser + range * tint.r;
+        color.g = lesser + range * tint.g;
+        color.b = lesser + range * tint.b;
+    }
+    gl_FragColor = color;
+}`
