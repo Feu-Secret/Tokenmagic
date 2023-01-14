709643ebc1ab92d9949a0d0692b5e67bb53299fc
Aedif <vaitkus.martynas@gmail.com>
2023-01-14 22:42:31 +0000

    - Adding new filters to zOrder table - Gave new filters more consistent names

--- tokenmagic/fx/filters/FilterAscii.js
+++ tokenmagic/fx/filters/FilterAscii.js
@@ -5,7 +5,7 @@ export class FilterAscii extends PIXI.filters.AsciiFilter {
   constructor(params) {
     super();
     this.size = 8;
-    this.zOrder = 40;
+    this.zOrder = 310;
     this.animated = {};
     this.setTMParams(params);
     if (!this.dummy) {
